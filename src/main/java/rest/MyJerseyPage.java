2cc903b527c9a5b4c70e4fd661708ff026da936b
Shafi <shafikulambil@gmail.com>
2018-04-05 23:11:59 +0530

    jersey-helloworld
    jersey-helloworld

---
+++ src/main/java/rest/MyJerseyPage.java
@@ -0,0 +1,17 @@
+package rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class MyJerseyPage {
+
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String sayHtmlHello() {
+		return "Hello from Jersey GET HTML";
+	}
+
+}
